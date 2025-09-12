df007045221f6d670bff391a55321f1f72ec5bbd
Victor Castelan <victorcash7@gmail.com>
2025-09-12 10:59:55 -0700

    feat(cyfrin-sec3): integrate Chainlink ETH/USD price feed into FundMe

--- src/cyfrin-sec3/FundMe.sol
+++ src/cyfrin-sec3/FundMe.sol
@@ -1,11 +1,21 @@
-// SPDX-License-Identifier
+// SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 
 contract FundMe {
   function fund() public payable {
     require(msg.value > 1e18, "Must send greater than 1 eth");
   }
 
-  function withdraw() {}
+  function withdraw() public {}
+
+  function getPrice() public view returns(uint256) {
+    AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    (,int256 answer,,,) = dataFeed.latestRoundData();
+    return uint256(answer) * 1e10;
+  }
 }
+
