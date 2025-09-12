bdc9ed7d96996b41aad0631f8e359ecc29cf9932
Victor Castelan <victorcash7@gmail.com>
2025-09-12 11:32:42 -0700

    feat(cyfrin-sec3): add FundMe contract with USD min funding and price feed conversion

--- src/cyfrin-sec3/FundMe.sol
+++ src/cyfrin-sec3/FundMe.sol
@@ -6,8 +6,15 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 
 contract FundMe {
+  uint256 public minimumUSD = 5e18;
+
+  address[] public funders;
+  mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
   function fund() public payable {
-    require(msg.value > 1e18, "Must send greater than 1 eth");
+    require(getConversionRate(msg.value) > minimumUSD, "Must send greater than 5 USD");
+    funders.push(msg.sender);
+    addressToAmountFunded[msg.sender] += msg.value;
   }
 
   function withdraw() public {}
@@ -16,6 +23,11 @@ contract FundMe {
     AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
     (,int256 answer,,,) = dataFeed.latestRoundData();
     return uint256(answer) * 1e10;
+  }
+
+  function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+    uint256 ethPrice = getPrice();
+    return (ethPrice * ethAmount) / 1e18;
   }
 }
 
