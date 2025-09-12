8c54dfcb155a2d616750e2535ea69ecee6ac53e4
Victor Castelan <victorcash7@gmail.com>
2025-09-12 12:22:42 -0700

    feat(cyfrin-sec3): refactor FundMe to use PriceConverter library and add withdraw function

--- src/cyfrin-sec3/FundMe.sol
+++ src/cyfrin-sec3/FundMe.sol
@@ -2,32 +2,28 @@
 
 pragma solidity 0.8.19;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+  using PriceConverter for uint256;
+
   uint256 public minimumUSD = 5e18;
 
   address[] public funders;
   mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
   function fund() public payable {
-    require(getConversionRate(msg.value) > minimumUSD, "Must send greater than 5 USD");
+    require(msg.value.getConversionRate() > minimumUSD, "Must send greater than 5 USD");
     funders.push(msg.sender);
     addressToAmountFunded[msg.sender] += msg.value;
   }
 
-  function withdraw() public {}
+  function withdraw() public {
 
-  function getPrice() public view returns(uint256) {
-    AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-    (,int256 answer,,,) = dataFeed.latestRoundData();
-    return uint256(answer) * 1e10;
-  }
-
-  function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-    uint256 ethPrice = getPrice();
-    return (ethPrice * ethAmount) / 1e18;
+    for(uint256 index = 0; index < funders.length; index++) {
+      address funderAddress = funders[index];
+      addressToAmountFunded[funderAddress] = 0;
+    }
   }
 }
 
