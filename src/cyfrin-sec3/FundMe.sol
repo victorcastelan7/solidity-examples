0b6f3c17eb645ca863e3ec20f6fe2b9f11125a5c
Victor Castelan <victorcash7@gmail.com>
2025-09-12 14:00:40 -0700

    feat(cyfrin-sec3): add owner-only withdraw, custom errors, and tighten FundMe; integrate PriceConverter

--- src/cyfrin-sec3/FundMe.sol
+++ src/cyfrin-sec3/FundMe.sol
@@ -4,26 +4,46 @@ pragma solidity 0.8.19;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
+  address public immutable i_owner;
+
+  constructor() {
+    i_owner = msg.sender;
+  }
+
   using PriceConverter for uint256;
 
-  uint256 public minimumUSD = 5e18;
+  uint256 public MINIMUM_USD = 5e18;
 
   address[] public funders;
   mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
   function fund() public payable {
-    require(msg.value.getConversionRate() > minimumUSD, "Must send greater than 5 USD");
+    require(msg.value.getConversionRate() > MINIMUM_USD, "Must send greater than 5 USD");
     funders.push(msg.sender);
     addressToAmountFunded[msg.sender] += msg.value;
   }
 
-  function withdraw() public {
+  function withdraw() public onlyOwner {
 
     for(uint256 index = 0; index < funders.length; index++) {
       address funderAddress = funders[index];
       addressToAmountFunded[funderAddress] = 0;
     }
+
+    funders = new address[](0);
+
+      // payable(msg.sender).transfer(address(this).balance);
+    (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+    require (callSuccess, "call failed");
   }
+
+    modifier onlyOwner() {
+    if(msg.sender != i_owner) {revert NotOwner();}
+    _;
+  }
+
 }
 
